5a89ef33c1bd83f8802043d621d34522d5c72c7c
lucasrey <36405211+lucasrey@users.noreply.github.com>
2019-12-08 01:16:31 +0100

    edit app

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,35 @@
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config/database.config');
 
-var app = express();
+// on se connecte à la base de données
+mongoose.connect(config.url, { useNewUrlParser: true });
+const app = express();
 
-app.use(logger("dev"));
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const userRouter = require('./routes/user');
+const sensorsRouter = require('./routes/sensors');
+const sensorRouter = require('./routes/sensor');
+const measuresRouter = require('./routes/measures');
+const measureRouter = require('./routes/measure');
+
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use('/', indexRouter);
+app.use('/user', userRouter);
+app.use('/users', usersRouter);
+app.use('/sensor', sensorRouter);
+app.use('/sensors', sensorsRouter);
+app.use('/measure', measureRouter);
+app.use('/measures', measuresRouter);
 
 module.exports = app;
